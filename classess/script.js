88fdd3ca1922d81f991409798dc7d291dc93b4c5
akarshCL <akarshg222@gmail.com>
2026-08-18 16:49:30 +0530

    classes

--- classess/script.js
+++ classess/script.js
@@ -1,160 +1,312 @@
-// // // // class
-// // // // constructor
-// // // //instance method
-// // // // get / set method , static method, private fields
+// // // // // // // // class
+// // // // // // // // constructor
+// // // // // // // //instance method
+// // // // // // // // get / set method , static method, private fields
 
 
-// // // class obn21{
-// // // constructor(name="Anonymous User",location=null,age=null){
-// // // this._name=name;
-// // // this._location=location;
-// // // this._age=age
-// // // }
-// // // kuchKare(){ // this is a instance method
-// // //     console.log(`Hi my name ${this._name} and im from ${this._location} `)
-// // // }
+// // // // // // // class obn21{
+// // // // // // // constructor(name="Anonymous User",location=null,age=null){
+// // // // // // // this._name=name;
+// // // // // // // this._location=location;
+// // // // // // // this._age=age
+// // // // // // // }
+// // // // // // // kuchKare(){ // this is a instance method
+// // // // // // //     console.log(`Hi my name ${this._name} and im from ${this._location} `)
+// // // // // // // }
 
-// // // get name(){
-// // //     return this._name.toUpperCase()
-// // // }
-
-
-// // // get location(){
-// // //     return this._location
-// // // }
+// // // // // // // get name(){
+// // // // // // //     return this._name.toUpperCase()
+// // // // // // // }
 
 
-// // // }
-
-// // // let student1=new obn21("kaushal","Noida");
-// // // let student2=new obn21("saurav","Accio");
-// // // let student3=new obn21();
-// // // console.log(student1.name,student1.location) // both are different
-// // // console.log(student2) // both are different
-// // // console.log(student3)
-// // // student1.kuchKare();
-// // // student3.kuchKare();// normally call ker rahe hai.....
-
-// // // // the difference between constructor and instance;
-
-// // // // . Whenever the object creates then automatically constructor gets called;
-// // // // instacnce method we have to call normally
-
-// // // // constructor > intialization purpose
-// // // // instance > Object ka behaviour
-
-// // // // constructor()
-// // // // instance() // it can be any name y because its a instance method we are making.
+// // // // // // // get location(){
+// // // // // // //     return this._location
+// // // // // // // }
 
 
+// // // // // // // }
 
+// // // // // // // let student1=new obn21("kaushal","Noida");
+// // // // // // // let student2=new obn21("saurav","Accio");
+// // // // // // // let student3=new obn21();
+// // // // // // // console.log(student1.name,student1.location) // both are different
+// // // // // // // console.log(student2) // both are different
+// // // // // // // console.log(student3)
+// // // // // // // student1.kuchKare();
+// // // // // // // student3.kuchKare();// normally call ker rahe hai.....
 
+// // // // // // // // the difference between constructor and instance;
 
-// // // example of getter in class....
+// // // // // // // // . Whenever the object creates then automatically constructor gets called;
+// // // // // // // // instacnce method we have to call normally
 
-// // class Data{
-// // constructor(fName,lastName){
-// //     this._firstName=fName;
-// //     this._lastName=lastName;
-// // }
-// // get fullName(){
-// // if(this._age>18){
-// //     return this._firstName + " " + this._lastName
-// // }
-// // }
-// // }
+// // // // // // // // constructor > intialization purpose
+// // // // // // // // instance > Object ka behaviour
 
-// // let s1=new Data("Umar","Husain")
-
-// // console.log(s1.fullName)
+// // // // // // // // constructor()
+// // // // // // // // instance() // it can be any name y because its a instance method we are making.
 
 
 
 
 
-// // example of setter
+// // // // // // // example of getter in class....
+
+// // // // // // class Data{
+// // // // // // constructor(fName,lastName){
+// // // // // //     this._firstName=fName;
+// // // // // //     this._lastName=lastName;
+// // // // // // }
+// // // // // // get fullName(){
+// // // // // // if(this._age>18){
+// // // // // //     return this._firstName + " " + this._lastName
+// // // // // // }
+// // // // // // }
+// // // // // // }
+
+// // // // // // let s1=new Data("Umar","Husain")
+
+// // // // // // console.log(s1.fullName)
 
 
-// // class Car{
-
-// //     constructor(brand){
-// //         this._name=brand
-// //     }
-// //     get name(){
-// //         return this._name.toUpperCase()
-// //     } // this is for read 
 
 
-// //     set name(value){
-// //         this._name=value
-// //     }// this is for update
 
-// // }
-// // // let data=
-
-// // const c1=new Car("BMW")
-// // console.log(c1.name)
-// // c1.name="audi"
-// // // c1.name="audi" >> set name("audi")
-// // console.log(c1.name)
+// // // // // // example of setter
 
 
-// class Std {
+// // // // // // class Car{
 
-//     constructor(name, age) {
-//         this._name = name
-//         this._age = age
-//     }
+// // // // // //     constructor(brand){
+// // // // // //         this._name=brand
+// // // // // //     }
+// // // // // //     get name(){
+// // // // // //         return this._name.toUpperCase()
+// // // // // //     } // this is for read 
+
+
+// // // // // //     set name(value){
+// // // // // //         this._name=value
+// // // // // //     }// this is for update
+
+// // // // // // }
+// // // // // // // let data=
+
+// // // // // // const c1=new Car("BMW")
+// // // // // // console.log(c1.name)
+// // // // // // c1.name="audi"
+// // // // // // // c1.name="audi" >> set name("audi")
+// // // // // // console.log(c1.name)
+
+
+// // // // // class Std {
+
+// // // // //     constructor(name, age) {
+// // // // //         this._name = name
+// // // // //         this._age = age
+// // // // //     }
 
     
 
-//    static intro(){
-//         return "Welcome to the MERN Class"
+// // // // //    static intro(){
+// // // // //         return "Welcome to the MERN Class"
+// // // // //     }
+// // // // //     get name() {
+// // // // //         return this._name.toUpperCase()
+// // // // //     } // this is for read 
+// // // // //     set name(value) {
+// // // // //         this._name = value
+// // // // //     }// this is for update
+// // // // //     set age(value) {
+// // // // //         if(value<0){
+// // // // //             console.log("Invalid Age");
+// // // // //             return;
+// // // // //         }
+// // // // //         this._age = value
+// // // // //     }
+// // // // // }
+
+// // // // // let s1=new Std("akarsh",27)
+// // // // // s1.age=29
+// // // // // console.log(s1)
+// // // // // console.log(s1.intro)// undefined
+// // // // // console.log(Std.intro()) // class ke ander
+
+
+// // // // // // object-Specific = Instance Method
+// // // // // // class-Specific= static method
+
+// // // // // // Math.max
+
+
+
+// // // // class BankAccount{
+// // // //     #balance;
+// // // //     #accName;
+// // // //     constructor(accName,balance){
+// // // //         this.#accName=accName
+// // // //         this.#balance=balance
+// // // //     }
+
+// // // //     getBalance(){
+// // // //        return this.#balance
+// // // //     }
+// // // // }
+
+// // // // let res=new BankAccount("kaushal",5000);
+
+// // // // // console.log(res.#balance)
+
+// // // // console.log(res.getBalance())
+
+// // // //==========================================
+
+
+// // // ////encapsulation=======================
+
+// // // class PersonBankAccount{
+// // // #balance;
+// // // constructor(initialAmount){
+// // //     this.#balance=initialAmount
+// // // }
+// // // deposit(amount){
+// // // if(amount>0){
+// // //     this.#balance += amount;
+// // // }
+// // // }
+// // // withDraw(amount){
+// // // if(amount>0 && this.#balance>=amount){
+// // //     this.#balance -= amount
+// // //     return `withdraw ${amount}`
+// // // }else{
+// // //     return "You don't have a enough rupee"
+// // // }
+// // // }
+// // // getBalance(){
+// // // return this.#balance
+// // // }
+// // // }
+
+// // // let res=new PersonBankAccount(1000);// opening bank balance
+// // // // res.#balance // we can't write
+// // // res.deposit(5000)
+// // // console.log(res.withDraw(2000))
+// // // console.log(res.getBalance())
+// // // // =======================================================
+
+
+
+
+// // class Payment{
+
+// //     pay(amount){
+
+// //         // amountchecker 
+// //         this.#validateAmount(amount);
+// //         this.#connectToBank()
+// //         this.#paymentProcess(amount)
+// //         // connectToBank
+// //         // Payment Process
+// //         console.log("payment successfull!!!!!")
+// //     }
+// //     #validateAmount(amount){
+// //         console.log("validating the amount......")
+// //     }
+// //     #connectToBank(){
+// //         console.log("connection to bank")
+// //     }
+// //     #paymentProcess(amount){
+// //         console.log("processing")
+// //     }
+// // }
+
+
+// // let paymentToPay=new Payment();
+
+
+// // paymentToPay.pay(500);
+
+
+
+
+
+// class BankAccount{
+//     #balance=0; // encapsulation
+
+//     deposite(amount){ // abstarction
+//         // validate
+//         this.#validate(amount)
+//         //balance update
+//         this.#balance +=amount;
 //     }
-//     get name() {
-//         return this._name.toUpperCase()
-//     } // this is for read 
-//     set name(value) {
-//         this._name = value
-//     }// this is for update
-//     set age(value) {
-//         if(value<0){
-//             console.log("Invalid Age");
-//             return;
-//         }
-//         this._age = value
+
+//     #validate(amount){
+//         // complax logic is running
 //     }
 // }
 
-// let s1=new Std("akarsh",27)
-// s1.age=29
-// console.log(s1)
-// console.log(s1.intro)// undefined
-// console.log(Std.intro()) // class ke ander
 
 
-// // object-Specific = Instance Method
-// // class-Specific= static method
+// // encapsulation => tp protect the data 
+// // abstarction=> to hide the complexity that i don't want to show the user
 
-// // Math.max
+//================================================================
 
 
 
-class BankAccount{
-    #balance;
-    #accName;
-    constructor(accName,balance){
-        this.#accName=accName
-        this.#balance=balance
+
+// class Trainer{
+//     constructor(name,age,phone){
+//         this.name=name;
+//         this.age=age;
+//         this.phone=phone
+//     }
+// }
+
+
+
+
+// class Student{
+//     constructor(name,age,phone){
+//         this.name=name;
+//         this.age=age;
+//         this.phone=phone
+//     }
+// }
+
+class Person{
+    constructor(name,age,phone){
+        this.name=name;
+        this.age=age;
+        this.phone=phone
     }
 
-    getBalance(){
-       return this.#balance
+    intro(){
+        console.log(`Hello ${this.name}`)
     }
+
+}
+class Student extends Person{
+    constructor(name,age,phone,course){
+      super(name,age,phone)
+      this.name="name";
+        this.course=course;
+    }
+    getData(){
+        console.log(this.course)
+    }
+
+    intro(){
+        super.intro()
+        console.log(`Hello ${this.name} how are you!`)
+    }
+
 }
 
-let res=new BankAccount("kaushal",5000);
+let s1=new Student("Saquib","30",101,"FSD")
 
-// console.log(res.#balance)
-
-console.log(res.getBalance())
+// s1.getData()
+console.log(s1.age)
+console.log(s1.name)
+s1.getData()
+s1.intro()
